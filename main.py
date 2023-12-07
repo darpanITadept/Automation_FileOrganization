491ab538c4e393ad208e1684220bf3d8ad53f5dc
darpan-itadept <darpan.itadept@gmail.com>
2023-12-06 23:43:05 -0500

    Added a feature to Add Custom Paths

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import file_handler
 
 # File Path
-path = '/Users/darpanchoudhary128/Documents/Test'
+default_path = '/Users/darpanchoudhary128/Documents/Test'
 
-def main():
+def main(path):
+    
+    # Asking user if they want to enter path of their choice or use the default path
+    while True:
+        yesNo = input("Do you want to choose path of your choice? (Y|N): ")
+        
+        if yesNo.upper() == 'Y':
+            userPath = input("Please enter your path: ")
+            path = userPath
+            break
+        elif yesNo.upper() == 'N':
+            print(f"Default Path Used! {path}")
+            break
+        else:
+            print("Try Again! Enter a Valid Input")
+        
+            
     try:
+        # Extracting file names in the directory
         print("File Names:")
         file_names = file_handler.list_directory_contents(path)
         
@@ -12,14 +29,18 @@ def main():
             print("No files found in the specified directory.")
             return  # Stop further execution if no files are found
         
+        # Extracting extensions of files in the directory
         print("File Extensions:")
         file_extensions = file_handler.list_file_extensions(file_names)
         
+        # Organizing files into respective Directories based on their extensions
         file_handler.organize_files_by_extension(path, file_extensions, file_names)
         print("Files Organized!")
         
     except Exception as e:
         print(f"An error occurred: {e}")
+        
+        
 
 if __name__ == '__main__':
-    main()
+    main(default_path)
